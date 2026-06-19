f87f98ddf2fad6c99817de5404e5ee63ca016f7e
Nandy Thaher Ulga <nandythaherulga20@gmail.com>
2026-06-19 16:10:09 +0700

    fix: add fallback url for mealdb

--- src/config/db.js
+++ src/config/db.js
@@ -1,15 +1,7 @@
 const axios = require('axios');
-const dotenv = require('dotenv');
-
-const axios = require('axios');
 
 const mealDB = axios.create({
   baseURL: process.env.MEALDB_BASE_URL || 'https://www.themealdb.com/api/json/v1/1',
-});
-
-module.exports = mealDB;
-const mealDB = axios.create({
-  baseURL: process.env.MEALDB_BASE_URL,
 });
 
 module.exports = mealDB;
